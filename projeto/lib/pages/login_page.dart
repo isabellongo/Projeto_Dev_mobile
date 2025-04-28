b9b8902deb36abab4a84245776c234c2c59aa9cc
adrilps <pauloadriano.2020.contato@gmail.com>
2025-04-28 14:43:04 -0300

    deleted unused code

--- projeto/lib/pages/login_page.dart
+++ projeto/lib/pages/login_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
